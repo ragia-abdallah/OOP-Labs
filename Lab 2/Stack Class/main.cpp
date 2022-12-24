c431b6adea62dfd29b1829d2077ee98a1bb33d5c
ragia_abdallah <ragiamabdallah@gmail.com>
2022-12-24 14:00:28 +0200

    Lab 2 adjustments

--- Lab 2/Stack Class/main.cpp
+++ Lab 2/Stack Class/main.cpp
@@ -10,19 +10,33 @@ class Stack
     int tos;
     int* st;
 
+    static int counter;
+
     public:
 
-    Stack(int s=10)
+    Stack()
+    {
+        tos = 0;
+        Size = 0;
+        st = NULL;
+        counter++;
+        cout<<"Stack "<<counter<<"has been created"<<endl;
+    };
+
+    Stack(int s)
     {
         tos = 0;
         Size = s;
         st = new int[Size];
+        counter++;
+        cout<<"Stack "<<counter<<"has been created"<<endl;
     };
 
     ~Stack()
     {
         delete[] st;
-        cout<<"Stack has been discarded"<<endl;
+        counter--;
+        cout<<"Stack has been discarded. Total is "<<counter<<endl;
     };
 
     void push(int x)
@@ -53,14 +67,58 @@ class Stack
         return top;
     };
 
+    int getCounter()
+    {
+        return counter;
+    };
+
 };
 
+int Stack::counter = 0;
 
 int main()
 {
+    //cout<<Stack::getCounter()<<endl;
 
+    Stack s1(2);
 
+    s1.push(5);
+    s1.push(14);
+    s1.push(20);
 
-    cout << "Hello world!" << endl;
+    cout<<s1.pop();
+
+    Stack s2;
+
+    s2.push(3);
+
+    cout<<s1.getCounter()<<endl;
+
+    cout<<"Gimme size of s3:"<<endl;
+    int sz3;
+    cin>>sz3;
+
+    Stack s3(sz3);
+
+    int i = 0;
+
+    while(i<sz3)
+    {
+        i++;
+        s3.push(i);
+    }
+
+    cout<<"Let's count down!"<<endl;
+
+    i = 0;
+
+    while(i<sz3)
+    {
+        cout<<s3.pop()<<endl;
+        i++;
+    }
+
+    cout << "Bye bye world!" << endl;
+
     return 0;
 }
